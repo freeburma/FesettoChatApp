dcae3bd61d54bca54364db13ba2600fb374b10e4
freeburma <kophyoblue@gmail.com>
2017-09-20 18:24:40 +1200

    Work Up to Tutorial 15 25:04

---
+++ FesettoChatApp.Core/ViewModel/Chat/ChatMessage/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// Design-time data for a <see cref="ChatMessageListItemViewModel"/>
+    /// </summary>
+    public class ChatMessageListItemDesignModel : ChatMessageListItemViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instanc eof the design model. 
+        /// </summary>
+        // public static ChatMessageListItemDesignModel Instance { get { return new ChatMessageListItemDesignModel();  } } // Same as below
+        public static ChatMessageListItemDesignModel Instance => new ChatMessageListItemDesignModel(); 
+
+        #endregion
+
+        #region Constructor 
+
+        /// <summary>
+        /// Default contructor 
+        /// </summary>
+        public ChatMessageListItemDesignModel()
+        {
+            Initials = "LM";
+            SenderName = "Luke";
+            Message = "Some design time visual text";
+            ProfilePictureRGB = "3099c5";
+            SendByMe = true;
+            MessageSentTime = DateTimeOffset.UtcNow;
+            MessageReadTime = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1.3));
+
+        }// end constructor()
+
+        #endregion
+
+
+    }// end class
+}
---
+++ FesettoChatApp.Core/ViewModel/Chat/ChatMessage/Design/ChatMessageListDesignModel.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// Design-time data for a <see cref="ChatMessageListViewModel"/>
+    /// </summary>
+    public class ChatMessageListDesignModel : ChatMessageListViewModel
+    {
+        #region Singleton
+
+        /// <summary>
+        /// A single instanc eof the design model. 
+        /// </summary>
+        // public static ChatListItemDesignModel Instance { get { return new ChatListItemDesignModel();  } } // Same as below
+        public static ChatMessageListDesignModel Instance => new ChatMessageListDesignModel(); 
+
+        #endregion
+
+        #region Constructor 
+
+        /// <summary>
+        /// Default contructor 
+        /// </summary>
+        public ChatMessageListDesignModel()
+        {
+            /*
+                // The following code is as same as this. 
+
+                Items = new List<ChatListItemViewModel> (); 
+                var inst = new ChatListItemViewModel(); 
+                inst.Name = "Luke";
+                inst.Initials = "LM";
+                inst. = "This chat app is awesome! I bet it will be fast too.";
+                inst.ProfilePictureRGB = "3099c5";
+
+                Items.add(inst); 
+            */
+            Items = new List<ChatMessageListItemViewModel>
+            {
+                new ChatMessageListItemViewModel
+                {
+                    SenderName = "Parnell",
+                    Initials = "PL",
+                    Message = "I'm about to wipe the old server. We need to update the old server to Windows 2016",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    SendByMe = false,
+                },
+                new ChatMessageListItemViewModel
+                {
+                    SenderName = "Luke",
+                    Initials = "LM",
+                    Message = "Let me know when you manage to spin up the new 2016 server",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    MessageReadTime = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1.3)),
+                    SendByMe = true,
+                },
+                new ChatMessageListItemViewModel
+                {
+                    SenderName = "Parnell",
+                    Initials = "PL",
+                    Message = "The new server is up. Go to 192.168.1.1. \r\nUser name admin, password is P8ssw0rd!",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    SendByMe = false,
+                },
+            }; 
+        }// end constructor()
+
+        #endregion
+
+
+    }// end class
+}
